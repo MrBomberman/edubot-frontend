6417ed8eb16240390c152aa361596eca8f6bdaaa
Kirill <mrbomberman99@gmail.com>
2023-06-27 21:50:36 +0300

    added base login route and home route

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,50 @@
-import { useState } from 'react'
-import './App.css'
+// import { Provider } from "react-redux";
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom"
+// import CircleLoader from "./components/CircleLoader";
+// import NavigationPanel from "./components/NavigationPanel";
+// import PrivateRoute from "./components/PrivateRoute";
+// import HistoryPage from "./pages/HistoryPage";
+// import KVGroupPage from "./pages/KVGroupPage";
+import LoginPage from "./pages/LoginPage";
+// import PageWithListOfAnyStatusKVGroup from "./pages/PageWithListOfAnyStatusKVGroup";
+// import NotFound from "./pages/NotFound";
+// import {store} from './store/store.js'
 
-function App() {
-  const [count, setCount] = useState(0)
 
+function App() : JSX.Element {
   return (
-    <>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    // <Provider>
+      <Router>
+        <Routes>
+          <Route exact path='/login' element={<LoginPage/>} />
+
+          <Route exact path="/" element={<div>Hello</div>}/>
+
+          {/* <Route exact path='/new' element={<PrivateRoute>
+            <PageWithListOfAnyStatusKVGroup statusPage={'New'}/>
+          </PrivateRoute>} />       
+
+          <Route exact path='/trash' element={<PrivateRoute>
+            <PageWithListOfAnyStatusKVGroup statusPage={'Trash'}/>
+          </PrivateRoute>} />
+
+          <Route exact path='/confirmed' element={<PrivateRoute>
+            <PageWithListOfAnyStatusKVGroup statusPage={'Confirmed'}/>
+          </PrivateRoute>} />
+
+          <Route exact path='/hold' element={<PrivateRoute>
+            <PageWithListOfAnyStatusKVGroup statusPage={'Hold'}/>
+          </PrivateRoute>} />
+
+          <Route exact path='/history' element={<PrivateRoute><HistoryPage/></PrivateRoute>} />
+
+          <Route exact path='/keygroup/:id' element={<PrivateRoute><KVGroupPage/></PrivateRoute>} /> */}
+          {/* <Route path='*' element={<NotFound/>}/> */}
+        </Routes>
+      
+      </Router>
+    // </Provider>
+  );
 }
 
-export default App
+export default App;
