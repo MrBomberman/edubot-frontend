dbb5072d2cd57e9df67d30b27962eb03e2fa2aa7
Kirill <mrbomberman99@gmail.com>
2023-07-05 17:48:51 +0300

    refactored chat ui component

--- src/components/ChatUI/index.tsx
+++ src/components/ChatUI/index.tsx
@@ -1,4 +1,4 @@
-import * as React from "react";
+import { useState, useRef, useEffect } from "react";
 import {
   Box,
   TextField,
@@ -9,26 +9,73 @@ import {
   Paper,
 } from "@mui/material";
 import SendIcon from "@mui/icons-material/Send";
+import postMessage from "../../api/post-data/postMessage";
+import { LoadingButton } from "@mui/lab";
+import robot from '../../images/robot.png';
 
 const messages = [
-  { id: 1, text: "Hi there!", sender: "bot" },
-  { id: 2, text: "Hello!", sender: "user" },
-  { id: 3, text: "How can I assist you today?", sender: "bot" },
+  { id: Math.random()*2 , text: "Hello dear Student, welcome to your personalized tutoring program. Our team consists of world class tutors who will guide you to excel and be among the top 1% of students worldwide. Our main focus will be the Cambridge Academic program, preparing you for IGCSE exams.", sender: "assistant" },  { id: Math.random()*2 , text: "Hello dear Student, welcome to your personalized tutoring program. Our team consists of world class tutors who will guide you to excel and be among the top 1% of students worldwide. Our main focus will be the Cambridge Academic program, preparing you for IGCSE exams.", sender: "assistant" },
+  { id: Math.random()*2 , text: "Hello dear Student, welcome to your personalized tutoring program. Our team consists of world class tutors who will guide you to excel and be among the top 1% of students worldwide. Our main focus will be the Cambridge Academic program, preparing you for IGCSE exams.", sender: "assistant" },
+  { id: Math.random()*2 , text: "Hello dear Student, welcome to your personalized tutoring program. Our team consists of world class tutors who will guide you to excel and be among the top 1% of students worldwide. Our main focus will be the Cambridge Academic program, preparing you for IGCSE exams.", sender: "assistant" },
+  { id: Math.random()*2 , text: "Hello dear Student, welcome to your personalized tutoring program. Our team consists of world class tutors who will guide you to excel and be among the top 1% of students worldwide. Our main focus will be the Cambridge Academic program, preparing you for IGCSE exams.", sender: "assistant" },
+  { id: Math.random()*2 , text: "Hello dear Student, welcome to your personalized tutoring program. Our team consists of world class tutors who will guide you to excel and be among the top 1% of students worldwide. Our main focus will be the Cambridge Academic program, preparing you for IGCSE exams.", sender: "assistant" },
+  { id: Math.random()*2 , text: "Hello dear Student, welcome to your personalized tutoring program. Our team consists of world class tutors who will guide you to excel and be among the top 1% of students worldwide. Our main focus will be the Cambridge Academic program, preparing you for IGCSE exams.", sender: "assistant" },
+  { id: Math.random()*2 , text: "Hello dear Student, welcome to your personalized tutoring program. Our team consists of world class tutors who will guide you to excel and be among the top 1% of students worldwide. Our main focus will be the Cambridge Academic program, preparing you for IGCSE exams.", sender: "assistant" },
+  { id: Math.random()*2 , text: "Hello dear Student, welcome to your personalized tutoring program. Our team consists of world class tutors who will guide you to excel and be among the top 1% of students worldwide. Our main focus will be the Cambridge Academic program, preparing you for IGCSE exams.", sender: "assistant" },
+  { id: Math.random()*2 , text: "Hello dear Student, welcome to your personalized tutoring program. Our team consists of world class tutors who will guide you to excel and be among the top 1% of students worldwide. Our main focus will be the Cambridge Academic program, preparing you for IGCSE exams.", sender: "assistant" },
+  { id: Math.random()*2 , text: "Hello dear Student, welcome to your personalized tutoring program. Our team consists of world class tutors who will guide you to excel and be among the top 1% of students worldwide. Our main focus will be the Cambridge Academic program, preparing you for IGCSE exams.", sender: "assistant" },
+
 ];
 
 const ChatUI = () => {
-  const [input, setInput] = React.useState("");
+  const [input, setInput] = useState<string>("");
+  const [loading, setLoading] = useState<boolean>(false);
+
+  const messageBlockRef = useRef({});
 
   const handleSend = () => {
     if (input.trim() !== "") {
-      console.log(input);
+      setLoading(true)
+      messages.push({id: (Math.random()*3), text: input, sender: 'user'})
+      postMessage("https://bostonbackendengine-sc4x4pjhiq-uc.a.run.app/api/v1/boston/chatbot-reference", input)
+        .then((res) => {
+          messageBlockRef.current.scrollTo(0, messageBlockRef.current.scrollHeight)
+          const textMessageObj = res.filter((item : any) => item.role == 'assistant');
+          const messageFromBot = {id: (Math.random()*2), text: textMessageObj[0].content, sender: textMessageObj[0].role};
+          messages.push(messageFromBot)
+          setLoading(false);
+        })
       setInput("");
     }
   };
 
-  const handleInputChange = (event) => {
+  const handleInputChange = (event: any) => {
     setInput(event.target.value);
   };
+
+  const handleKeyDown = (event: any) => {
+    if (input.trim() !== "") {
+      // messageBlockRef.current.scrollTo({top: messageBlockRef.current.scrollHeight})
+      if(event.key == 'Enter'){
+        setLoading(true)
+        messages.push({id: (Math.random()*3), text: input, sender: 'user'})
+        postMessage("https://bostonbackendengine-sc4x4pjhiq-uc.a.run.app/api/v1/boston/chatbot-reference", input)
+          .then((res) => {
+            messageBlockRef?.current.scrollTo(0, messageBlockRef?.current.scrollHeight)
+            console.log('Data: ', res)
+            const textMessageObj = res.filter((item : any) => item.role == 'assistant');
+            const messageFromBot = {id: (Math.random()*2), text: textMessageObj[0].content, sender: textMessageObj[0].role};
+            messages.push(messageFromBot)
+            setLoading(false);
+          })
+        setInput("");
+      }
+    }
+  }
+
+  useEffect(() => {
+      messageBlockRef.current.scrollTo({top: messageBlockRef.current.scrollHeight, behavior: 'smooth'})
+  }, [messageBlockRef.current.scrollHeight])
 
   return (
     <Box
@@ -39,7 +86,7 @@ const ChatUI = () => {
         bgcolor: "grey.200",
       }}
     >
-      <Box sx={{ flexGrow: 1, overflow: "auto", p: 2 }}>
+      <Box ref={messageBlockRef} sx={{ flexGrow: 1, overflow: "auto", p: 2 }}>
         {messages.map((message) => (
           <Message key={message.id} message={message} />
         ))}
@@ -54,18 +101,21 @@ const ChatUI = () => {
               variant="outlined"
               value={input}
               onChange={handleInputChange}
+              onKeyUp={handleKeyDown}
+              disabled={loading ? true : false}
             />
           </Grid>
           <Grid item xs={2}>
-            <Button
+            <LoadingButton
               fullWidth
               color="primary"
               variant="contained"
+              loading={loading}
               endIcon={<SendIcon />}
               onClick={handleSend}
             >
               Send
-            </Button>
+            </LoadingButton>
           </Grid>
         </Grid>
       </Box>
@@ -74,7 +124,7 @@ const ChatUI = () => {
 };
 
 const Message = ({ message }) => {
-  const isBot = message.sender === "bot";
+  const isBot = message.sender === "assistant";
 
   return (
     <Box
@@ -91,7 +141,8 @@ const Message = ({ message }) => {
           alignItems: "center",
         }}
       >
-        <Avatar sx={{ bgcolor: isBot ? "primary.main" : "secondary.main" }}>
+        <Avatar sx={{ bgcolor: isBot ? "primary.main" : "secondary.main"}}
+          src={isBot ? robot : ""}>
           {isBot ? "B" : "U"}
         </Avatar>
         <Paper
@@ -100,7 +151,7 @@ const Message = ({ message }) => {
             p: 2,
             ml: isBot ? 1 : 0,
             mr: isBot ? 0 : 1,
-            backgroundColor: isBot ? "primary.light" : "secondary.light",
+            backgroundColor: isBot ? "primary.light" : "#FFF",
             borderRadius: isBot ? "20px 20px 20px 5px" : "20px 20px 5px 20px",
           }}
         >
