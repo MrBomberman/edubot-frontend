5e3ead5e93d53f484b307af60ec159ee3c9f1380
Kirill <mrbomberman99@gmail.com>
2023-07-18 11:19:09 +0300

    commented code for styles mobile

--- src/components/BookImageSlider/index.tsx
+++ src/components/BookImageSlider/index.tsx
@@ -20,6 +20,22 @@ import CircularProgress from '@mui/material/CircularProgress';
 //   imgPath: string
 // }
 
+// const stylesMoBile = (theme) => ({
+
+//   root: {
+//     padding: theme.spacing(1),
+//     [theme.breakpoints.down('md')]: {
+//       backgroundColor: theme.palette.secondary.main,
+//     },
+//     [theme.breakpoints.up('md')]: {
+//       backgroundColor: theme.palette.primary.main,
+//     },
+//     [theme.breakpoints.up('lg')]: {
+//       backgroundColor: green[500],
+//     },
+//   },
+// });
+
 export default function BookImageSlider() {
   const theme = useTheme();
   const [activeStep, setActiveStep] = useState<number>(0);
