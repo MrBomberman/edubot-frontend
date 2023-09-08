a966f652a4378a9af6f8effbc2ad28ba5927f438
Kirill <mrbomberman99@gmail.com>
2023-09-08 17:31:42 +0300

    added necessary text to block of cards

--- src/components/PresentationComponents/BlockOfCardsOnPresPage/index.tsx
+++ src/components/PresentationComponents/BlockOfCardsOnPresPage/index.tsx
@@ -1,42 +1,67 @@
-import {Box, Grid} from '@mui/material'
+import { Box, Grid } from "@mui/material";
+import ResponsiveCard from "../../../shared/common/ResponsiveCard";
+import useMediaQuery from "@mui/material/useMediaQuery";
 
 const steps = [
-    {
-      label: 'Tailored Curriculum',
-      description: `Exclusively adapted for Cambridge Assessment International Education. Cover every key topic seamlessly.`,
-    },
-    {
-      label: '24/7 Availability',
-      description:
-        'From late-night queries to early morning revisions, Boston is here to support you non-stop.',
-    },
-    {
-      label: 'Adaptive Learning',
-      description: `Boston understands your unique learning pace, adjusting content for optimal comprehension and retention.`,
-    },
-    {
-        label: 'Absolute Privacy',
-        description: `Rest easy knowing your data and progress are safeguarded with top-tier encryption.`,
-      },
-  ];
+  {
+    label: "On-the-Go Q&A",
+    description: `Immediate answers to your burning questions, anytime, anywhere.`,
+  },
+  {
+    label: "Cambridge Mock Test Environment",
+    description: `Simulate the real exam vibe with Boston's AI-driven mock tests.`,
+  },
+  {
+    label: "Real-time Progress Insights",
+    description: `Witness your growth, highlight strengths, and focus on areas awaiting mastery.`,
+  },
+  {
+    label: "Dynamic Learning Modules",
+    description: `Dive into a range of content - from riveting video lessons to quick quizzes and recap flashcards.`,
+  },
+];
 
-export default function BlockOfCardsOnPresPage(){
-    return (
-        <Box>
-            <Grid container sx={{minHeight: "400px", p: 5, fontFamily: 'DM Sans',
-                    display: "flex",
-                    alignItems: "center",
-                    justifyContent: "center"}}>
-                {
-                    steps.map((label, i) => {
-                        return (
-                            <Grid item xs={12} md={6}>
-                                {label.description}
-                            </Grid>
-                        )
-                    })
-                }
+export default function BlockOfCardsOnPresPage() {
+
+  return (
+    <Box sx={{ marginTop: 15 }}>
+      <Box component={"h2"} sx={{ textAlign: "center", fontFamily: "DM Sans" }}>
+        What can you get with Boston the Edubot?
+      </Box>
+      <Grid
+        container
+        sx={{
+          minHeight: "400px",
+          p: 5,
+          fontFamily: "DM Sans",
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+        }}
+      >
+        {steps.map((label, i) => {
+          return (
+            <Grid
+              item
+              sm={12}
+              md={6}
+              lg={3}
+              spacing={4}
+              sx={{
+                display: "flex",
+                alignItems: "center",
+                justifyContent: "center",
+              }}
+            >
+              <ResponsiveCard
+                title={label.label}
+                text={label.description}
+                responsiveCard={false}
+              />
             </Grid>
-        </Box>
-    )
+          );
+        })}
+      </Grid>
+    </Box>
+  );
 }
