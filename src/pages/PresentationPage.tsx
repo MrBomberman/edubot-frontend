bc6ba4dd6cc23b06dd802612923b7bc2ec22b24a
Kirill <mrbomberman99@gmail.com>
2023-09-11 17:44:11 +0300

    added enw components to presentation page

--- src/pages/PresentationPage.tsx
+++ src/pages/PresentationPage.tsx
@@ -1,5 +1,7 @@
 import BlockOfCardsOnPresPage from '../components/PresentationComponents/BlockOfCardsOnPresPage';
+import CommentsBlock from '../components/PresentationComponents/CommentsBlock';
 import MainMessageBlock from '../components/PresentationComponents/MainMessageBlock';
+import PresentationFooter from '../components/PresentationComponents/PresentationFooter';
 import PresentationHeader from '../components/PresentationComponents/PresentationHeader';
 
 function PresentationPage() {
@@ -9,6 +11,8 @@ function PresentationPage() {
         <PresentationHeader/>
         <MainMessageBlock/>
         <BlockOfCardsOnPresPage/>
+        <CommentsBlock/>
+        <PresentationFooter/>
     </>
   );
 }
